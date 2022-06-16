fefae717003703ee20a48dc3f5848975a20b0a64
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 09:03:22 -0700

    finish FactoryConfiguration class

---
+++ src/com/hibernate/hostel_management_system/util/FactoryConfiguration.java
@@ -0,0 +1,39 @@
+package com.hibernate.hostel_management_system.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/16/2022
+ * Time        : 8:55 AM
+ */
+public class FactoryConfiguration {
+
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+
+    private FactoryConfiguration() throws IOException {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        configuration.setProperties(properties);
+        //configuration.addAnnotatedClass();
+        sessionFactory=configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() throws IOException {
+        return (factoryConfiguration == null) ? factoryConfiguration=new FactoryConfiguration()
+                : factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
