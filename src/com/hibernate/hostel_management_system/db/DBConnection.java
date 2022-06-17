f14de9215db7a33413fedc49d65f53c90518e84e
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 22:36:15 -0700

    create DBConnection class

---
+++ src/com/hibernate/hostel_management_system/db/DBConnection.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.db;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/16/2022
+ * Time        : 10:35 PM
+ */
+public class DBConnection {
+}
