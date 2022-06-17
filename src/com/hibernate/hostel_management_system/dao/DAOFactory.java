f0699e55505361990ed924145c24e7218e9b8039
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 00:39:19 -0700

    create DAOFactory class

---
+++ src/com/hibernate/hostel_management_system/dao/DAOFactory.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.dao;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/17/2022
+ * Time        : 12:38 AM
+ */
+public class DAOFactory {
+}
