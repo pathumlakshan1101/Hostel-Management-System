792123e705b901ef350e75846b35e3945e003f8a
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 22:59:13 -0700

    create SuperBO class

---
+++ src/com/hibernate/hostel_management_system/bo/SuperBO.java
@@ -0,0 +1,12 @@
+package com.hibernate.hostel_management_system.bo;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/16/2022
+ * Time        : 10:58 PM
+ * @Since : 0.1.0
+ */
+
+public interface SuperBO {
+}
