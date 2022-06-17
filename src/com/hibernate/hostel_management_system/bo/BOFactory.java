853d10bfcc37ce92cec4ceec4981d304ab253214
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 23:19:12 -0700

    create BOFactory class

---
+++ src/com/hibernate/hostel_management_system/bo/BOFactory.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.bo;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/16/2022
+ * Time        : 11:02 PM
+ */
+public class BOFactory {
+}
