49713ab6da00e396bee73db46c5aea8152fa51c6
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 10:20:28 -0700

    create  AppInitializer java fx class

---
+++ src/com/hibernate/hostel_management_system/AppInitializer.java
@@ -0,0 +1,16 @@
+package com.hibernate.hostel_management_system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
