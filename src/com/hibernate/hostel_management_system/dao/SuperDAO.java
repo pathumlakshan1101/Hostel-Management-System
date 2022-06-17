4a5e6f82fb3c0ca7eaea06045d5e1cfb8fa36bb1
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 00:07:11 -0700

    create SuperDAO class

---
+++ src/com/hibernate/hostel_management_system/dao/SuperDAO.java
@@ -0,0 +1,12 @@
+package com.hibernate.hostel_management_system.dao;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/17/2022
+ * Time        : 12:03 AM
+ * @Since : 0.1.0
+ */
+
+public interface SuperDAO {
+}
