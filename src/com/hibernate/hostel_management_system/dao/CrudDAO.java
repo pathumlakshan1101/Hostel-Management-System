ad4eae3895b8b3a91e29d22eb79908bac52cc5cb
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 00:31:48 -0700

    create crud method

---
+++ src/com/hibernate/hostel_management_system/dao/CrudDAO.java
@@ -0,0 +1,31 @@
+package com.hibernate.hostel_management_system.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/17/2022
+ * Time        : 12:10 AM
+ * @Since : 0.1.0
+ */
+
+public interface CrudDAO <T,ID> extends SuperDAO{
+
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    boolean save(T dto) throws SQLException, ClassNotFoundException;
+
+    boolean update(T dto) throws SQLException, ClassNotFoundException;
+
+    T search(ID id)throws SQLException,ClassNotFoundException;
+
+    boolean exist(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    String generateNewID() throws SQLException, ClassNotFoundException;
+
+
+}
