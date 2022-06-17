60962f75d3891cef032725daa6ba6cab806ca22b
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 10:44:31 -0700

    implement id an on Action method sign in form to controller

--- src/com/hibernate/hostel_management_system/controller/SignFormController.java
+++ src/com/hibernate/hostel_management_system/controller/SignFormController.java
@@ -1,5 +1,11 @@
 package com.hibernate.hostel_management_system.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
@@ -7,4 +13,11 @@ package com.hibernate.hostel_management_system.controller;
  * Time        : 3:17 AM
  */
 public class SignFormController {
+    public AnchorPane signPagesContex;
+
+    public void initialize() throws IOException {
+        signPagesContex.getChildren().clear();
+        Parent parent = FXMLLoader.load(SignFormController.class.getResource("../view/fxml/sign/SignInForm.fxml"));
+        signPagesContex.getChildren().add(parent);
+    }
 }
--- src/com/hibernate/hostel_management_system/controller/SignInFormController.java
+++ src/com/hibernate/hostel_management_system/controller/SignInFormController.java
@@ -1,5 +1,11 @@
 package com.hibernate.hostel_management_system.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
@@ -7,4 +13,21 @@ package com.hibernate.hostel_management_system.controller;
  * Time        : 10:25 AM
  */
 public class SignInFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField pswdfldPassword;
+    public JFXTextField txtPassword;
+    public ImageView imgVisibilityOn;
+    public ImageView imgVisibilityOff;
+
+    public void SignInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void visibilityOnMaouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void visibilityOnMaouseExited(MouseEvent mouseEvent) {
+    }
 }
