770c4ac35dc4a6f0792f08ddbb742cc440e26bc5
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 22:52:34 -0700

    finish implement logics to SQLUtil class

--- src/com/hibernate/hostel_management_system/dao/SQLUtil.java
+++ src/com/hibernate/hostel_management_system/dao/SQLUtil.java
@@ -1,5 +1,10 @@
 package com.hibernate.hostel_management_system.dao;
 
+import com.hibernate.hostel_management_system.db.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
@@ -7,4 +12,17 @@ package com.hibernate.hostel_management_system.dao;
  * Time        : 10:41 PM
  */
 public class SQLUtil {
+
+    public static <T> T execute(String sql, Object...params) throws SQLException, ClassNotFoundException {
+        PreparedStatement statement =
+                DBConnection.getInstance().getConnection().prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            statement.setObject((i+1),params[i]);
+        }
+        if (sql.startsWith("SELECT")){
+            return (T) statement.executeQuery();
+        }else{
+            return ((T)(Boolean)(statement.executeUpdate()>0));
+        }
+    }
 }
