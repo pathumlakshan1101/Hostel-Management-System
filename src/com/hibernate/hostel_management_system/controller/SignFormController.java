4f757f8168fb1a11d93c6ad9f6333ab327c39b44
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 03:18:57 -0700

    create SignFormController class

---
+++ src/com/hibernate/hostel_management_system/controller/SignFormController.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.controller;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/17/2022
+ * Time        : 3:17 AM
+ */
+public class SignFormController {
+}
