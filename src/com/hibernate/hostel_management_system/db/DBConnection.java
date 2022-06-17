4acf890ac9ac55bc20b530f52262e99f79c14710
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 22:38:44 -0700

    finish create dbconnection

--- src/com/hibernate/hostel_management_system/db/DBConnection.java
+++ src/com/hibernate/hostel_management_system/db/DBConnection.java
@@ -1,5 +1,9 @@
 package com.hibernate.hostel_management_system.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
@@ -7,4 +11,16 @@ package com.hibernate.hostel_management_system.db;
  * Time        : 10:35 PM
  */
 public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+    private  DBConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameCafe", "root", "1101");
+    }
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection==null)? dbConnection= new DBConnection(): dbConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
 }
