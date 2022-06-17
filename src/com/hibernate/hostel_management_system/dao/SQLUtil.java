b6752bfa0d69b3df25db838764550b07bde6e00d
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-16 22:43:02 -0700

    create SQLUtil class

---
+++ src/com/hibernate/hostel_management_system/dao/SQLUtil.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.dao;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/16/2022
+ * Time        : 10:41 PM
+ */
+public class SQLUtil {
+}
