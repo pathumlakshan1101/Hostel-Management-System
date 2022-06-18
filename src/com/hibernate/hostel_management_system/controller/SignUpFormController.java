9fb1740b1c2669bd207e86d986b1554ec95ee909
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 23:47:29 -0700

    create sign up page and implement it's method and id to controller class

--- src/com/hibernate/hostel_management_system/controller/SignUpFormController.java
+++ src/com/hibernate/hostel_management_system/controller/SignUpFormController.java
@@ -1,5 +1,11 @@
 package com.hibernate.hostel_management_system.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
@@ -7,4 +13,27 @@ package com.hibernate.hostel_management_system.controller;
  * Time        : 8:49 PM
  */
 public class SignUpFormController {
+    public JFXTextField txtFullName;
+    public JFXPasswordField pswdfldPassword;
+    public JFXTextField txtPassword;
+    public ImageView imgVisibilityOn;
+    public ImageView imgVisibilityOff;
+    public JFXTextField txtContactNo;
+    public JFXTextField txtEmail;
+
+    public void initialize(){
+
+    }
+
+    public void signUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void visibilityOnMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void visibilityOnMouseExited(MouseEvent mouseEvent) {
+    }
 }
