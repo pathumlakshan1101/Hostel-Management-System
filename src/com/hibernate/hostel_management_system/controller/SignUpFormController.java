5fc3fc7b6c9ad1c0087f07ee3565aecf84a45dd5
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-17 20:50:46 -0700

    create signUp fxml form and controller class

---
+++ src/com/hibernate/hostel_management_system/controller/SignUpFormController.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.controller;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/17/2022
+ * Time        : 8:49 PM
+ */
+public class SignUpFormController {
+}
