4c0c61e03991773e2c34bfec45d59666ec0b5902
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-18 02:23:47 -0700

    create DashBoard fxml And Controller Class

---
+++ src/com/hibernate/hostel_management_system/controller/DashBoardFormController.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.controller;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/18/2022
+ * Time        : 2:22 AM
+ */
+public class DashBoardFormController {
+}
