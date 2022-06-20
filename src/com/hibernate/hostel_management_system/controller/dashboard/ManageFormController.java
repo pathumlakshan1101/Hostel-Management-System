665b183595ddc63e8b6934aa328aa5618a3fc3cf
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-20 10:29:30 -0700

    create manage form and controller class

---
+++ src/com/hibernate/hostel_management_system/controller/dashboard/ManageFormController.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.controller.dashboard;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/20/2022
+ * Time        : 10:28 AM
+ */
+public class ManageFormController {
+}
