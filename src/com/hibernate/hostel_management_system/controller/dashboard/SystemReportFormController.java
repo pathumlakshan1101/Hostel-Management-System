1e5f95f531a6e8927e5001598558cf9cc2b499dd
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-22 03:37:18 -0700

    create system Report form and controller

---
+++ src/com/hibernate/hostel_management_system/controller/dashboard/SystemReportFormController.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.controller.dashboard;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/22/2022
+ * Time        : 3:36 AM
+ */
+public class SystemReportFormController {
+}
