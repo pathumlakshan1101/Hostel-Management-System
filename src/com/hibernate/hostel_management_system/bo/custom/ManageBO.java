11926a7aa1c1d85e0eba5d31eb9d164a95dedc31
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 22:53:13 +0530

    Create BO interfaces

---
+++ src/com/hibernate/hostel_management_system/bo/custom/ManageBO.java
@@ -0,0 +1,12 @@
+package com.hibernate.hostel_management_system.bo.custom;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/23/2022
+ * Time        : 10:51 PM
+ * @Since : 0.1.0
+ */
+
+public interface ManageBO {
+}
