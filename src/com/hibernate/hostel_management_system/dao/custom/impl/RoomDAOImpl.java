455b15b779274327bcb1a73eb86289733c74587e
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 14:53:42 +0530

    implement RoomDAO and override method for RoomDAOImpl class

--- src/com/hibernate/hostel_management_system/dao/custom/impl/RoomDAOImpl.java
+++ src/com/hibernate/hostel_management_system/dao/custom/impl/RoomDAOImpl.java
@@ -1,10 +1,50 @@
 package com.hibernate.hostel_management_system.dao.custom.impl;
 
+import com.hibernate.hostel_management_system.dao.custom.RoomDAO;
+import com.hibernate.hostel_management_system.entity.Room;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
  * Date        : 6/23/2022
  * Time        : 2:41 PM
  */
-public class RoomDAOImpl {
+public class RoomDAOImpl implements RoomDAO {
+    @Override
+    public ArrayList<Room> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(Room dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Room dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Room search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
