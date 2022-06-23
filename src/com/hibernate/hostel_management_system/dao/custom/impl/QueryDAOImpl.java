56cd0390051e4f1a28b8aeb11c2bf30872497d87
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 15:05:47 +0530

    implement QueryDao for QueryDaoImpl

--- src/com/hibernate/hostel_management_system/dao/custom/impl/QueryDAOImpl.java
+++ src/com/hibernate/hostel_management_system/dao/custom/impl/QueryDAOImpl.java
@@ -1,10 +1,12 @@
 package com.hibernate.hostel_management_system.dao.custom.impl;
 
+import com.hibernate.hostel_management_system.dao.custom.QueryDAO;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
  * Date        : 6/23/2022
  * Time        : 2:44 PM
  */
-public class QueryDAOImpl {
+public class QueryDAOImpl implements QueryDAO {
 }
