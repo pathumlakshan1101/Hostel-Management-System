b5bed20f82bf5b56649cd43e82745769e591510f
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 14:51:32 +0530

    implement studentDao for studentDaoImpl class and override method

--- src/com/hibernate/hostel_management_system/dao/custom/impl/StudentDAOImpl.java
+++ src/com/hibernate/hostel_management_system/dao/custom/impl/StudentDAOImpl.java
@@ -1,10 +1,50 @@
 package com.hibernate.hostel_management_system.dao.custom.impl;
 
+import com.hibernate.hostel_management_system.dao.custom.StudentDAO;
+import com.hibernate.hostel_management_system.entity.Student;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
  * Date        : 6/23/2022
  * Time        : 2:42 PM
  */
-public class StudentDAOImpl {
+public class StudentDAOImpl implements StudentDAO {
+    @Override
+    public ArrayList<Student> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(Student dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Student dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Student search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
