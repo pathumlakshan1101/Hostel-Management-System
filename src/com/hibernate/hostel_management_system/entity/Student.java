564296dfb5a4e5a6d5c9f195c5faf2b63d871aff
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 09:08:32 +0530

    create Student Entity Class

--- src/com/hibernate/hostel_management_system/entity/Student.java
+++ src/com/hibernate/hostel_management_system/entity/Student.java
@@ -1,10 +1,94 @@
 package com.hibernate.hostel_management_system.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
  * Date        : 6/23/2022
  * Time        : 8:54 AM
  */
+
+@Entity
 public class Student {
+        @Id
+        private String studentID;
+        private String studentName;
+        private String studentAddress;
+        private String studentContact;
+        private String dateOfBirth;
+        private String gender;
+
+    public Student(String studentID, String studentName, String studentAddress, String studentContact, String dateOfBirth, String gender) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+        this.studentContact = studentContact;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+
+    public Student() {
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public String getStudentContact() {
+        return studentContact;
+    }
+
+    public void setStudentContact(String studentContact) {
+        this.studentContact = studentContact;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentID='" + studentID + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", studentAddress='" + studentAddress + '\'' +
+                ", studentContact='" + studentContact + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
 }
