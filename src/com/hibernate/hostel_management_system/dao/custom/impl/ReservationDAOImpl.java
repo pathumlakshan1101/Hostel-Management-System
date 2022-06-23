4ebe36a58080cdfc4a48b7f2c39e989a16a96641
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 15:04:48 +0530

    implement ReservationDao and override method for ReservationDaoImpl

--- src/com/hibernate/hostel_management_system/dao/custom/impl/ReservationDAOImpl.java
+++ src/com/hibernate/hostel_management_system/dao/custom/impl/ReservationDAOImpl.java
@@ -1,10 +1,49 @@
 package com.hibernate.hostel_management_system.dao.custom.impl;
 
+import com.hibernate.hostel_management_system.dao.custom.ReservationDAO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * @author : ALE_IS_TER
  * Project Name: Hostel_Management_System
  * Date        : 6/23/2022
  * Time        : 2:40 PM
  */
-public class ReservationDAOImpl {
+public class ReservationDAOImpl implements ReservationDAO {
+    @Override
+    public ArrayList<ReservationDAO> getAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(ReservationDAO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(ReservationDAO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public ReservationDAO search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
