8c56927506d9a2a6f47270e85fd548bd6e961f9f
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 08:55:33 +0530

    create student entity class

---
+++ src/com/hibernate/hostel_management_system/entity/Student.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.entity;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/23/2022
+ * Time        : 8:54 AM
+ */
+public class Student {
+}
