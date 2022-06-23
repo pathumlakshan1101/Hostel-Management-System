1acd00b6218a0790f2ac91baa459162b0bd309c4
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-23 16:14:28 +0530

    create forget form fxml and controller

---
+++ src/com/hibernate/hostel_management_system/controller/sign/ForgetFormController.java
@@ -0,0 +1,10 @@
+package com.hibernate.hostel_management_system.controller.sign;
+
+/**
+ * @author : ALE_IS_TER
+ * Project Name: Hostel_Management_System
+ * Date        : 6/23/2022
+ * Time        : 4:08 PM
+ */
+public class ForgetFormController {
+}
