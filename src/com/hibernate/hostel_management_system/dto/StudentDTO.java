da1bc23097296f2f7032209851c72e4e5b0fe24a
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-29 15:34:01 +0530

    implement lambok to Student Dto class

--- src/com/hibernate/hostel_management_system/dto/StudentDTO.java
+++ src/com/hibernate/hostel_management_system/dto/StudentDTO.java
@@ -2,6 +2,7 @@ package com.hibernate.hostel_management_system.dto;
 
 import com.hibernate.hostel_management_system.entity.Reservation;
 import com.hibernate.hostel_management_system.entity.User;
+import lombok.*;
 
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -14,6 +15,14 @@ import java.util.List;
  * Date        : 6/25/2022
  * Time        : 10:19 AM
  */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+
 public class StudentDTO {
     private String studentID;
     private String studentName;
@@ -33,105 +42,4 @@ public class StudentDTO {
         this.gender = gender;
     }
 
-    public StudentDTO(String studentID, String studentName, String studentAddress, String studentContact, String dateOfBirth, String gender, String userName) {
-        this.studentID = studentID;
-        this.studentName = studentName;
-        this.studentAddress = studentAddress;
-        this.studentContact = studentContact;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.userName = userName;
-    }
-
-    public StudentDTO(String studentID, String studentName, String studentAddress, String studentContact, String dateOfBirth, String gender, String userName, List<Reservation> reservationList) {
-        this.studentID = studentID;
-        this.studentName = studentName;
-        this.studentAddress = studentAddress;
-        this.studentContact = studentContact;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.userName = userName;
-        this.reservationList = reservationList;
-    }
-
-    public StudentDTO() {
-    }
-
-    public String getStudentID() {
-        return studentID;
-    }
-
-    public void setStudentID(String studentID) {
-        this.studentID = studentID;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getStudentAddress() {
-        return studentAddress;
-    }
-
-    public void setStudentAddress(String studentAddress) {
-        this.studentAddress = studentAddress;
-    }
-
-    public String getStudentContact() {
-        return studentContact;
-    }
-
-    public void setStudentContact(String studentContact) {
-        this.studentContact = studentContact;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public List<Reservation> getReservationList() {
-        return reservationList;
-    }
-
-    public void setReservationList(List<Reservation> reservationList) {
-        this.reservationList = reservationList;
-    }
-
-    @Override
-    public String toString() {
-        return "StudentDTO{" +
-                "studentID='" + studentID + '\'' +
-                ", studentName='" + studentName + '\'' +
-                ", studentAddress='" + studentAddress + '\'' +
-                ", studentContact='" + studentContact + '\'' +
-                ", dateOfBirth='" + dateOfBirth + '\'' +
-                ", gender='" + gender + '\'' +
-                ", userName='" + userName + '\'' +
-                ", reservationList=" + reservationList +
-                '}';
-    }
 }
