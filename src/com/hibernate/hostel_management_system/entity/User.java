989a75d33a5672189e2f48a96a86460b50a54182
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-29 15:30:30 +0530

    implement lambok to User entity class

--- src/com/hibernate/hostel_management_system/entity/User.java
+++ src/com/hibernate/hostel_management_system/entity/User.java
@@ -1,5 +1,7 @@
 package com.hibernate.hostel_management_system.entity;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -13,6 +15,14 @@ import java.util.List;
  * Date        : 6/23/2022
  * Time        : 9:08 AM
  */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+
 @Entity
 public class User {
     @Id
@@ -24,14 +34,6 @@ public class User {
     private
     List<Student> studentList = new ArrayList<>();
 
-    public User(String userName, String contactNo, String email, String password, List<Student> studentList) {
-        this.userName = userName;
-        this.contactNo = contactNo;
-        this.email = email;
-        this.password = password;
-        this.studentList = studentList;
-    }
-
     public User(String userName, String contactNo, String email, String password) {
         this.userName = userName;
         this.contactNo = contactNo;
@@ -39,57 +41,5 @@ public class User {
         this.password = password;
     }
 
-    public User() {
-    }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getContactNo() {
-        return contactNo;
-    }
-
-    public void setContactNo(String contactNo) {
-        this.contactNo = contactNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Student> getStudentList() {
-        return studentList;
-    }
-
-    public void setStudentList(List<Student> studentList) {
-        this.studentList = studentList;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userName='" + userName + '\'' +
-                ", contactNo='" + contactNo + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", studentList=" + studentList +
-                '}';
-    }
 }
