04c5427d40db9cd237cb2865d7a267b54ffb9218
lakshanheratg <lakshanherath1101@gmail.com>
2022-06-29 15:28:44 +0530

    implement lambok to Student entity class

--- src/com/hibernate/hostel_management_system/entity/Student.java
+++ src/com/hibernate/hostel_management_system/entity/Student.java
@@ -1,5 +1,7 @@
 package com.hibernate.hostel_management_system.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,13 @@ import java.util.List;
  * Date        : 6/23/2022
  * Time        : 8:54 AM
  */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
 
 @Entity
 public class Student {
@@ -29,16 +38,6 @@ public class Student {
         this.studentID = studentID;
     }
 
-    public Student(String studentID, String studentName, String studentAddress, String studentContact, String dateOfBirth, String gender, User user, List<Reservation> reservationList) {
-        this.studentID = studentID;
-        this.studentName = studentName;
-        this.studentAddress = studentAddress;
-        this.studentContact = studentContact;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.user = user;
-        this.reservationList = reservationList;
-    }
 
     public Student(String studentID, String studentName, String studentAddress, String studentContact, String dateOfBirth, String gender) {
         this.studentID = studentID;
@@ -49,84 +48,4 @@ public class Student {
         this.gender = gender;
     }
 
-    public Student() {
-    }
-
-    public String getStudentID() {
-        return studentID;
-    }
-
-    public void setStudentID(String studentID) {
-        this.studentID = studentID;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getStudentAddress() {
-        return studentAddress;
-    }
-
-    public void setStudentAddress(String studentAddress) {
-        this.studentAddress = studentAddress;
-    }
-
-    public String getStudentContact() {
-        return studentContact;
-    }
-
-    public void setStudentContact(String studentContact) {
-        this.studentContact = studentContact;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public List<Reservation> getReservationList() {
-        return reservationList;
-    }
-
-    public void setReservationList(List<Reservation> reservationList) {
-        this.reservationList = reservationList;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "studentID='" + studentID + '\'' +
-                ", studentName='" + studentName + '\'' +
-                ", studentAddress='" + studentAddress + '\'' +
-                ", studentContact='" + studentContact + '\'' +
-                ", dateOfBirth='" + dateOfBirth + '\'' +
-                ", gender='" + gender + '\'' +
-                ", user=" + user +
-                ", reservationList=" + reservationList +
-                '}';
-    }
 }
